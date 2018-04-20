cb4922fa93d8631ab5ceda58c0699a221314703b
Yuena Chen <yuenachen93@gmail.com>
2018-04-20 18:05:32 -0400

    add insert query

--- modify/server/models/database.js
+++ modify/server/models/database.js
@@ -7,7 +7,7 @@ var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "1234",
-    database : "QMPS"
+    database : "QualityMilkDB"
 });
 
 
@@ -30,10 +30,10 @@ var web = function () {};
 //     };
 web.prototype.get_Teat = function(date1,date2,callback){
     let sql = "select farm_id, date, staff_id, milker_id, isBefore, clean, deep, small_dirt, large_dirt " +
-                "from cleanliness_fact where date >= Date(?) and date <= Date(?);";
-    console.log(date1+date2);
+                "from cleanliness_fact where date >= date1 and date <= date2;";
+    console.log(date1);
     var result = [];
-    con.query(sql, [date1, date2],function(err, res){
+    con.query(sql, [date1],function(err, res){
         if (err) return callback(err);
         if (res.length){
             for (var i = 0; i < res.length; i++){
@@ -47,10 +47,10 @@ web.prototype.get_Teat = function(date1,date2,callback){
 
 web.prototype.get_udder = function(date1,date2,callback){
     let sql = "select group_id, staff_id, date, farm_id, free, slightly, moderate, heavy " +
-                "from udder_hygiene_fact where date >= Date(?) and date <= Date(?);";
-    console.log(date1+date2);
+                "from udder_hygiene_fact where date >= date1 and date <= date2;";
+    console.log(date1);
     var result = [];
-    con.query(sql, [date1, date2],function(err, res){
+    con.query(sql, [date1],function(err, res){
         if (err) return callback(err);
         if (res.length){
             for (var i = 0; i < res.length; i++){
@@ -64,10 +64,10 @@ web.prototype.get_udder = function(date1,date2,callback){
 
 web.prototype.get_unit = function(date1,date2,callback){
     let sql = "select farm_id, date, good, bad " +
-                "from unit_fact where date >= Date(?) and date <= Date(?)";
-    console.log(date1+date2);
+                "from unit_fact where date >= date1 and date <= date2;";
+    console.log(date1);
     var result = [];
-    con.query(sql, [date1, date2],function(err, res){
+    con.query(sql, [date1],function(err, res){
         if (err) return callback(err);
         if (res.length){
             for (var i = 0; i < res.length; i++){
@@ -82,10 +82,10 @@ web.prototype.get_unit = function(date1,date2,callback){
 
 web.prototype.get_strip = function(date1,date2,callback){
     let sql = "select staff_id, date, farm_id, ML, Stall_no, isBalanced " +
-                "from strip_fact where date >= Date(?) and date <= Date(?);";
-    console.log(date1+date2);
+                "from strip_fact where date >= date1 and date <= date2;";
+    console.log(date1);
     var result = [];
-    con.query(sql, [date1, date2],function(err, res){
+    con.query(sql, [date1],function(err, res){
         if (err) return callback(err);
         if (res.length){
             for (var i = 0; i < res.length; i++){
@@ -103,10 +103,10 @@ web.prototype.get_post = function(date1,date2,callback){
               "LF_skin_condition, LF_color, LF_swelling, LF_hardness, LF_end_score, " +
               "RH_skin_condition, RH_color, RH_swelling, RH_hardness, RH_end_score, " +
               "RF_skin_condition, RF_color, RF_swelling, RF_hardness, RF_end_score, " +
-                "from postmilking_fact where date >= Date(?) and date <= Date(?);";
-    console.log(date1+date2);
+                "from postmilking_fact where date >= date1 and date <= date2;";
+    console.log(date1);
     var result = [];
-    con.query(sql, [date1, date2],function(err, res){
+    con.query(sql, [date1],function(err, res){
         if (err) return callback(err);
         if (res.length){
             for (var i = 0; i < res.length; i++){
@@ -123,10 +123,10 @@ web.prototype.get_lacto = function(date1,date2,callback){
                 "parlor_type, pre_milking,procedures,milking_frequency,staff_id,prep, " +
                 "milking_routine, total_milk, herd_size,size,dip_contact_time, lag_contact_stimulate, unit_on_time " +
                 "from  lactocoder_fact " +
-                "where date >= Date(?) and date <= Date(?);";
-    console.log(date1+date2);
+                "where date >= date1 and date <= date2;";
+    console.log(date1);
     var result = [];
-    con.query(sql, [date1, date2],function(err, res){
+    con.query(sql, [date1],function(err, res){
         if (err) return callback(err);
         if (res.length){
             for (var i = 0; i < res.length; i++){
@@ -135,14 +135,123 @@ web.prototype.get_lacto = function(date1,date2,callback){
         }
         callback(null, result);
     });
-
 }
 
 
 
 
+web.prototype.post_lacto = function(json, callback){
+    let sql = "insert into cleanliness_fact ( group_id, staff_id, milker_id, " +
+                    "date, farm_id, isBefore, clean, deep, small_dirt, large_dirt)" +
+                    "values ?";
+    var temp = [];
+    var array = [];
+    for (i in json){
+        temp = [];
+        for (var key in json[i]){
+            temp.push(file[i][key]);
+        }
+        array.push(temp);
+    }
+    con.query(sql,[array] function(err, res)){
+        if (err) return callback(err);
+        callback(null, res);
+    }
+}
 
 
+web.prototype.post_udder = function(json, callback){
+    let sql = "insert into  udder_hygiene_fact(group_id, staff_id,date,farm_id,free,slightly,moderate,heavy)" +
+                    "values ?";
+    var temp = [];
+    var array = [];
+    for (i in json){
+        temp = [];
+        for (var key in json[i]){
+            temp.push(file[i][key]);
+        }
+        array.push(temp);
+    }
+    con.query(sql,[array] function(err, res)){
+        if (err) return callback(err);
+        callback(null, res);
+    }
+}
+
+web.prototype.post_unit = function(json, callback){
+    let sql = "insert into unit_fact(date, farm_id, good, bad) values ?;";
+    var temp = [];
+    var array = [];
+    for (i in json){
+        temp = [];
+        for (var key in json[i]){
+            temp.push(file[i][key]);
+        }
+        array.push(temp);
+    }
+    con.query(sql,[array] function(err, res)){
+        if (err) return callback(err);
+        callback(null, res);
+    }
+}
+
+
+web.prototype.post_strip = function(json, callback){
+    let sql = "insert into strip_fact (staff_id,date,farm_id ,ML ,Stall_no,isBalanced) values ?;";
+    var temp = [];
+    var array = [];
+    for (i in json){
+        temp = [];
+        for (var key in json[i]){
+            temp.push(file[i][key]);
+        }
+        array.push(temp);
+    }
+    con.query(sql,[array] function(err, res)){
+        if (err) return callback(err);
+        callback(null, res);
+    }
+}
+
+web.prototype.post_post = function(json, callback){
+    let sql = "insert into postmilking_fact(staff_id, farm_id, group_id, date, LH_skin_condition," +
+     "LH_color, LH_swelling, LH_hardness, LH_end_score, LF_skin_condition, LF_color, LF_swelling," +
+    "LF_hardness, LF_end_score, RH_skin_condition, RH_color, RH_swelling, RH_hardness," + 
+     "RH_end_score, RF_skin_condition, RF_color, RF_swelling, RF_hardness, RF_end_score) values ?;";
+    var temp = [];
+    var array = [];
+    for (i in json){
+        temp = [];
+        for (var key in json[i]){
+            temp.push(file[i][key]);
+        }
+        array.push(temp);
+    }
+    con.query(sql,[array] function(err, res)){
+        if (err) return callback(err);
+        callback(null, res);
+    }
+}
+
+web.prototype.post_lacto = function(json, callback){
+    let sql = "insert into lactocoder_fact (staff_id,farm_id, parlor_type,size,no_operators,pre_milking,procedures," +
+                    "prep,herd_size,milking_frequency,milking_routine,dates, dip,strip,wipe,attach,remove_,dip_contact_time," +
+                    "lag_contact_stimulate,unit_on_time,total_milk,remark) " +
+                    "values ?";
+    var temp = [];
+    var array = [];
+    for (i in json){
+        temp = [];
+        for (var key in json[i]){
+            temp.push(file[i][key]);
+        }
+        array.push(temp);
+    }
+    con.query(sql,[array] function(err, res)){
+        if (err) return callback(err);
+        callback(null, res);
+    }
+}
 
 // database.prototype.get_Teat = function(date, callback){
 //     let sql = "select * from cleanliness_fact where date = ?";
